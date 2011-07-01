1baf2645ac7f62972c9e27981dc5144f0fe99782
Karl Adam <karladam@mac.com>
2011-07-01 14:23:00 -0700

    - Add a .xcodeproj to build a framework and static library

---
+++ TwUITests/TwUITests.h
@@ -0,0 +1,13 @@
+//
+//  TwUITests.h
+//  TwUITests
+//
+//  Created by Karl Adam on 11.07.01.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TwUITests : SenTestCase
+
+@end
---
+++ TwUITests/TwUITests.m
@@ -0,0 +1,32 @@
+//
+//  TwUITests.m
+//  TwUITests
+//
+//  Created by Karl Adam on 11.07.01.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TwUITests.h"
+
+@implementation TwUITests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TwUITests");
+}
+
+@end
