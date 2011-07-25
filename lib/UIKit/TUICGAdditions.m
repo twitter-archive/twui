007703a207cea1302cf6e05b43e77ef9c18a9f46
Loren Brichter <loren.brichter@gmail.com>
2011-07-25 18:06:58 -0400

    merge "Wrong arc parameter corrected" from zhangchn <zhangchnxp@gmail.com>

--- lib/UIKit/TUICGAdditions.m
+++ lib/UIKit/TUICGAdditions.m
@@ -66,7 +66,7 @@ void CGContextAddRoundRect(CGContextRef context, CGRect rect, CGFloat radius)
 	
 	CGContextMoveToPoint(context, rect.origin.x, rect.origin.y + radius);
 	CGContextAddLineToPoint(context, rect.origin.x, rect.origin.y + rect.size.height - radius);
-	CGContextAddArc(context, rect.origin.x + radius, rect.origin.y + rect.size.height - radius, radius, M_PI / 4, M_PI / 2, 1);
+	CGContextAddArc(context, rect.origin.x + radius, rect.origin.y + rect.size.height - radius, radius, M_PI, M_PI / 2, 1);
 	CGContextAddLineToPoint(context, rect.origin.x + rect.size.width - radius, rect.origin.y + rect.size.height);
 	CGContextAddArc(context, rect.origin.x + rect.size.width - radius, rect.origin.y + rect.size.height - radius, radius, M_PI / 2, 0.0f, 1);
 	CGContextAddLineToPoint(context, rect.origin.x + rect.size.width, rect.origin.y + radius);
