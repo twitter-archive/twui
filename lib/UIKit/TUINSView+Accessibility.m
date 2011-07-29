f44ca0f30452bb2375d53079fc6044491a785b6a
joshaber <joshaber@gmail.com>
2011-07-29 12:02:02 -0700

    hey it kinda works!

--- lib/UIKit/TUINSView+Accessibility.m
+++ lib/UIKit/TUINSView+Accessibility.m
@@ -12,12 +12,12 @@
 
 @implementation TUINSView (Accessibility)
 
-//- (id)accessibilityHitTest:(NSPoint)point
-//{
-//	NSPoint windowPoint = [[self window] convertScreenToBase:point];
-//	NSPoint localPoint = [self convertPoint:windowPoint fromView:nil];
-//	return [rootView accessibilityHitTest:localPoint];
-//}
+- (id)accessibilityHitTest:(NSPoint)point
+{
+	NSPoint windowPoint = [[self window] convertScreenToBase:point];
+	NSPoint localPoint = [self convertPoint:windowPoint fromView:nil];
+	return [rootView accessibilityHitTest:localPoint];
+}
 
 - (BOOL)accessibilityIsIgnored
 {
@@ -37,16 +37,11 @@
 //	}
 //}
 
-- (id)accessibilityHitTest:(NSPoint)point
-{
-	return self;
-}
-
 - (NSArray *)accessibilityAttributeNames
 {
     static NSArray *attributes = nil;
     if(attributes == nil) {
-		attributes = [[NSArray alloc] initWithObjects:NSAccessibilityRoleAttribute, NSAccessibilityRoleDescriptionAttribute, NSAccessibilityFocusedAttribute, NSAccessibilityParentAttribute, NSAccessibilityWindowAttribute, NSAccessibilityTopLevelUIElementAttribute, NSAccessibilityPositionAttribute, NSAccessibilitySizeAttribute, nil];
+		attributes = [[NSArray alloc] initWithObjects:NSAccessibilityRoleAttribute, NSAccessibilityRoleDescriptionAttribute, NSAccessibilityFocusedAttribute, NSAccessibilityChildrenAttribute, NSAccessibilityParentAttribute, NSAccessibilityWindowAttribute, NSAccessibilityTopLevelUIElementAttribute, NSAccessibilityPositionAttribute, NSAccessibilitySizeAttribute, nil];
     }
 	
     return attributes;
@@ -57,25 +52,23 @@
     if([attribute isEqualToString:NSAccessibilityRoleAttribute]) {
 		return NSAccessibilityGroupRole;
     } else if([attribute isEqualToString:NSAccessibilityRoleDescriptionAttribute]) {
-		return NSAccessibilityRoleDescription(NSAccessibilityGroupRole, nil);
+		return NSAccessibilityRoleDescriptionForUIElement(self);
     } else if([attribute isEqualToString:NSAccessibilityFocusedAttribute]) {
-		// Just check if the app thinks we're focused.
 		id focusedElement = [NSApp accessibilityAttributeValue:NSAccessibilityFocusedUIElementAttribute];
 		return [NSNumber numberWithBool:[focusedElement isEqual:self]];
     } else if([attribute isEqualToString:NSAccessibilityParentAttribute]) {
 		return NSAccessibilityUnignoredAncestor(self.superview);
     } else if([attribute isEqualToString:NSAccessibilityWindowAttribute]) {
-		// We're in the same window as our parent.
 		return [self.superview accessibilityAttributeValue:NSAccessibilityWindowAttribute];
     } else if([attribute isEqualToString:NSAccessibilityTopLevelUIElementAttribute]) {
-		// We're in the same top level element as our parent.
 		return [self.superview accessibilityAttributeValue:NSAccessibilityTopLevelUIElementAttribute];
     } else if([attribute isEqualToString:NSAccessibilityPositionAttribute]) {
 		return [NSValue valueWithPoint:[[self window] convertBaseToScreen:[self convertPoint:self.bounds.origin toView:nil]]];
-		
     } else if([attribute isEqualToString:NSAccessibilitySizeAttribute]) {
 		return [NSValue valueWithSize:self.bounds.size];
-    } else {
+    } else if([attribute isEqualToString:NSAccessibilityChildrenAttribute]) {
+		return [NSArray arrayWithObject:self.rootView];
+	} else {
 		return nil;
     }
 }
@@ -83,7 +76,7 @@
 - (BOOL)accessibilityIsAttributeSettable:(NSString *)attribute
 {
     if([attribute isEqualToString:NSAccessibilityFocusedAttribute]) {
-		return NO; //[parent isFauxUIElementFocusable:self];
+		return NO; // TODO: should this be settable?
     } else {
 		return NO;
     }
@@ -92,7 +85,7 @@
 - (void)accessibilitySetValue:(id)value forAttribute:(NSString *)attribute
 {
     if([attribute isEqualToString:NSAccessibilityFocusedAttribute]) {
-		//		[parent fauxUIElement:self setFocus:value];
+		// TODO: should we set this?
     }
 }
 
--- lib/UIKit/TUIView+Accessibility.m
+++ lib/UIKit/TUIView+Accessibility.m
@@ -95,27 +95,100 @@
 
 #pragma mark NSAccessibility
 
-- (id)accessibilityAttributeValue:(NSString *)attribute
-{
-    if([attribute isEqualToString:NSAccessibilityParentAttribute]) {
-		return self.superview ? : self.nsView;
-    } else if([attribute isEqualToString:NSAccessibilityWindowAttribute]) {
-		return [self.superview ? : self.nsView accessibilityAttributeValue:NSAccessibilityWindowAttribute];
-    } else if([attribute isEqualToString:NSAccessibilityTopLevelUIElementAttribute]) {
-		return [self.superview ? : self.nsView accessibilityAttributeValue:NSAccessibilityTopLevelUIElementAttribute];
-    }  else if([attribute isEqualToString:NSAccessibilityPositionAttribute]) {
-		return [NSValue valueWithPoint:[self convertPoint:self.bounds.origin toView:nil]];
-    } else if([attribute isEqualToString:NSAccessibilitySizeAttribute]) {
-		return [NSValue valueWithSize:[self convertRect:self.bounds toView:nil].size];
-    } else {
-		return [super accessibilityAttributeValue:attribute];
-	}
-}
-
 - (id)accessibilityHitTest:(NSPoint)point
 {
 	TUIView *h = [self hitTest:point withEvent:nil];
 	return h;
+}
+
+- (BOOL)accessibilityIsIgnored
+{
+    return NO;
+}
+//
+//- (id)accessibilityAttributeValue:(NSString *)attribute 
+//{
+//    if([attribute isEqualToString:NSAccessibilityRoleAttribute]) {
+//		return NSAccessibilityGroupRole;
+//    } else if([attribute isEqualToString:NSAccessibilityRoleDescriptionAttribute]) {
+//		return NSAccessibilityRoleDescriptionForUIElement(self);
+//	} else if([attribute isEqualToString:NSAccessibilityChildrenAttribute]) {
+//		return [self.rootView accessibleSubviews];
+//	} else {
+//		return [super accessibilityAttributeValue:attribute];
+//	}
+//}
+
+- (NSArray *)accessibilityAttributeNames
+{
+    static NSArray *attributes = nil;
+    if(attributes == nil) {
+		attributes = [[NSArray alloc] initWithObjects:NSAccessibilityRoleAttribute, NSAccessibilityRoleDescriptionAttribute, NSAccessibilityFocusedAttribute, NSAccessibilityChildrenAttribute, NSAccessibilityParentAttribute, NSAccessibilityWindowAttribute, NSAccessibilityTopLevelUIElementAttribute, NSAccessibilityPositionAttribute, NSAccessibilitySizeAttribute, nil];
+    }
+	
+    return attributes;
+}
+
+- (id)accessibilityAttributeValue:(NSString *)attribute
+{
+    if([attribute isEqualToString:NSAccessibilityRoleAttribute]) {
+		return NSAccessibilityGroupRole;
+    } else if([attribute isEqualToString:NSAccessibilityRoleDescriptionAttribute]) {
+		return NSAccessibilityRoleDescriptionForUIElement(self);
+    } else if([attribute isEqualToString:NSAccessibilityFocusedAttribute]) {
+		id focusedElement = [NSApp accessibilityAttributeValue:NSAccessibilityFocusedUIElementAttribute];
+		return [NSNumber numberWithBool:[focusedElement isEqual:self]];
+    } else if([attribute isEqualToString:NSAccessibilityParentAttribute]) {
+		return NSAccessibilityUnignoredAncestor((id) self.superview ? : self.nsView);
+    } else if([attribute isEqualToString:NSAccessibilityWindowAttribute]) {
+		return [self.superview accessibilityAttributeValue:NSAccessibilityWindowAttribute];
+    } else if([attribute isEqualToString:NSAccessibilityTopLevelUIElementAttribute]) {
+		return [self.superview accessibilityAttributeValue:NSAccessibilityTopLevelUIElementAttribute];
+    } else if([attribute isEqualToString:NSAccessibilityPositionAttribute]) {
+		return [NSValue valueWithPoint:[[(NSView *)self.nsView window] convertBaseToScreen:[self frameInNSView].origin]];
+    } else if([attribute isEqualToString:NSAccessibilitySizeAttribute]) {
+		return [NSValue valueWithSize:self.bounds.size];
+    } else if([attribute isEqualToString:NSAccessibilityChildrenAttribute]) {
+		return [self accessibleSubviews];
+	} else {
+		return nil;
+    }
+}
+
+- (BOOL)accessibilityIsAttributeSettable:(NSString *)attribute
+{
+    if([attribute isEqualToString:NSAccessibilityFocusedAttribute]) {
+		return NO; // TODO: should this be settable?
+    } else {
+		return NO;
+    }
+}
+
+- (void)accessibilitySetValue:(id)value forAttribute:(NSString *)attribute
+{
+    if([attribute isEqualToString:NSAccessibilityFocusedAttribute]) {
+		// TODO: should we set this?
+    }
+}
+
+- (NSArray *)accessibilityActionNames
+{
+    return [NSArray array];
+}
+
+- (NSString *)accessibilityActionDescription:(NSString *)action
+{
+    return nil;
+}
+
+- (void)accessibilityPerformAction:(NSString *)action
+{
+	
+}
+
+- (id)accessibilityFocusedUIElement
+{
+    return NSAccessibilityUnignoredAncestor(self);
 }
 
 @end
