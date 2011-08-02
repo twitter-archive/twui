f826a861faeefc9e74d10d89f06778f1e61bef61
joshaber <joshaber@gmail.com>
2011-08-01 22:53:20 -0700

    starting work on TUIControl actually reflecting state changes

---
+++ lib/UIKit/TUIControl+Private.h
@@ -0,0 +1,13 @@
+//
+//  TUIControl+Private.h
+//  TwUI
+//
+//  Created by Josh Abernathy on 7/19/11.
+//  Copyright 2011 Maybe Apps, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TUIControl_Private : NSObject
+
+@end
---
+++ lib/UIKit/TUIControl+Private.m
@@ -0,0 +1,23 @@
+//
+//  TUIControl+Private.m
+//  TwUI
+//
+//  Created by Josh Abernathy on 7/19/11.
+//  Copyright 2011 Maybe Apps, LLC. All rights reserved.
+//
+
+#import "TUIControl+Private.h"
+
+@implementation TUIControl_Private
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
