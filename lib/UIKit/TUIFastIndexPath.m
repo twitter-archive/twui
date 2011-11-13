b0b2725a1055f05bc0a68de4d363df84b85cc74f
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-12 16:40:49 -0800

    Fix ARC precheck warnings in TUIFastIndexPath (in part by removing memory management methods from the singleton implementation)

--- lib/UIKit/TUIFastIndexPath.m
+++ lib/UIKit/TUIFastIndexPath.m
@@ -30,12 +30,8 @@ static struct TUIFastIndexPath_staticStruct {
 
 @interface TUIFastIndexPath_staticClass : TUIFastIndexPath
 @end
+
 @implementation TUIFastIndexPath_staticClass
-
-- (id)retain { return self; }
-- (id)autorelease { return self; }
-- (oneway void)release { }
-
 @end
 
 @implementation TUIFastIndexPath
@@ -57,7 +53,7 @@ static struct TUIFastIndexPath_staticStruct {
 {
 	if(section == 0) {
 		if(row < CACHE_COMMON_INDEX_PATHS) {
-			return (TUIFastIndexPath *)&CommonIndexPaths[row];
+			return (__bridge TUIFastIndexPath *)&CommonIndexPaths[row];
 		}
 	}
 	
